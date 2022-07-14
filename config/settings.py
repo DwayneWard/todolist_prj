11f10d0e4f42ca2f49719f519aafb9e4d32cc960
neony <neonyan15@gmail.com>
2022-07-15 00:26:16 +0300

    fix vk auth

--- config/settings.py
+++ config/settings.py
@@ -123,7 +123,7 @@ SOCIAL_AUTH_VK_OAUTH2_KEY = "8215623"
 SOCIAL_AUTH_VK_OAUTH2_SECRET = env("SOCIAL_AUTH_VK_OAUTH2_SECRET")
 SOCIAL_AUTH_VK_OAUTH2_SCOPE = ["email", "photos", "notify"]
 SOCIAL_AUTH_LOGIN_REDIRECT_URL = '/logged_in/'
-SOCIAL_AUTH_LOGIN_ERROR_URL = "/login-error/"
+SOCIAL_AUTH_LOGIN_ERROR_URL = "/"
 SOCIAL_AUTH_VK_OAUTH2_WHITELISTED_DOMAINS = ['vleontev.ga']
 
 # Internationalization
@@ -148,3 +148,9 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+}
