73feb503b80516079509d24e184886b9164c8040
neony <neonyan15@gmail.com>
2022-08-01 20:51:52 +0300

    fix

--- bot/serializers.py
+++ bot/serializers.py
@@ -9,13 +9,14 @@ class TgUserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = TgUser
-        read_only_fields = ("tg_id", "username", "user_id")
-        fields = ("tg_id", "username", "verification_code", "user_id")
+        read_only_fields = ("tg_id", "username", "user_id", "tg_chat_id")
+        fields = '__all__'
+        # fields = ("tg_id", "username", "verification_code", "user_id", "tg_chat_id")
 
     def validate(self, attrs):
         verification_code = attrs.get("verification_code")
         tg_user = TgUser.objects.filter(verification_code=verification_code).first()
         if not tg_user:
-            raise ValidationError({"verification_code": "field is incorrect"})
+            raise ValidationError({"verification_code": "Некорректное значение"})
         attrs["tg_user"] = tg_user
         return attrs
